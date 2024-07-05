5b3faf23059b51a316da61f4825b9697769997e9
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-07-05 13:20:37 -0300

    initial fetching working

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,32 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	import { onMount } from 'svelte';
+
+	let users = [];
+
+	onMount(async () => {
+		const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+		users = await res.json();
+	});
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+ <h1>Search Monsters</h1>
+<input
+      class="search-box"
+      type="search"
+      placeholder="Search Monsters"
+    />
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+		<div class="card-list">
+	{#each users as user}
+		<div class="card-container">
+      <img
+        alt={`monster ${user.name}`}
+        src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+      />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+    {/each}
+	</div>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
