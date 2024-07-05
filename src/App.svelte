5c2dd6b8aebce185f50c5260dae5c9f591e2aff1
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-07-05 15:33:41 -0300

    search input working, app complete

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,23 @@
   import { onMount } from "svelte";
 
   let users = [];
-  let searchTerm = "";
+  let searchTerm = undefined;
   let filteredMonsters = [];
 
   onMount(async () => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
     users = await res.json();
   });
-  function searchChange() {
-    return (filteredMonsters = users.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchTerm);
-    }));
-  }
+  // function searchChange() {
+  //   return (filteredMonsters = users.filter((monster) =>
+  //     monster.name.toLocaleLowerCase().includes(searchTerm)
+  //   ));
+  // }
+  $: filteredMonsters = searchTerm
+    ? users.filter((monster) =>
+        monster.name.toLocaleLowerCase().includes(searchTerm)
+      )
+    : users;
 </script>
 
 <main>
@@ -23,9 +28,21 @@
     type="search"
     placeholder="Search Monsters"
     bind:value={searchTerm}
-    on:input={searchChange}
   />
-
+  <!-- {#if filteredMonsters.length < 0} -->
+  <!-- <div class="card-list">
+      {#each users as user}
+        <div class="card-container">
+          <img
+            alt={`monster ${user.name}`}
+            src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+          />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      {/each}
+    </div> -->
+  <!-- {:else} -->
   <div class="card-list">
     {#each filteredMonsters as user}
       <div class="card-container">
@@ -38,4 +55,5 @@
       </div>
     {/each}
   </div>
+  <!-- {/if} -->
 </main>
